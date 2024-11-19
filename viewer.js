818d5ce180d295a98cde2d1a43d5f63d6ec53c9e
Trevor Hodsdon <mrtj458@gmail.com>
2024-11-18 17:42:26 -0700

    Restructured to use plugin-like system. Now called version 2.0

--- viewer.js
+++ viewer.js
@@ -1,88 +1,85 @@
-const offset = 20;
-let mediaIndex = 0;
-let mediaImages = [];
+class ImageHandler {
+  supportedSites = ["i.redd.it", "imgur.com"];
 
-let mouseX = 0;
-let mouseY = 0;
+  img = null;
 
-let controller = new AbortController();
-
-const img = document.createElement("img");
-img.style.position = "fixed";
-img.style.maxWidth = "50vw";
-img.style.maxHeight = "90vh";
-img.style.border = "2px solid gray";
-img.style.borderRadius = "3px";
-img.style.zIndex = "1000000";
-img.hidden = true;
-document.body.appendChild(img);
-
-const indexBox = document.createElement("div");
-indexBox.style.position = "fixed";
-indexBox.style.backgroundColor = "gray";
-indexBox.style.padding = "2px 4px";
-indexBox.style.color = "white";
-indexBox.style.fontSize = "12px";
-indexBox.style.textAlign = "center";
-indexBox.style.zIndex = "1000000";
-indexBox.hidden = true;
-document.body.appendChild(indexBox);
-
-const positionImage = () => {
-  if (mouseX > window.innerWidth * 0.5) {
-    img.style.left = `${mouseX - img.width - offset}px`;
-  } else {
-    img.style.left = `${mouseX + offset}px`;
+  canHandle = (data) => {
+    for (let site of this.supportedSites) {
+      if (data.url.includes(site)) {
+        return true;
+      }
+    }
+    return false;
   }
 
-  indexBox.style.left = `${mouseX - indexBox.clientWidth / 2}px`;
+  display = (postData, mouseX, mouseY) => {
+    const img = document.createElement("img");
 
-  if (mouseY > window.innerHeight * 0.5) {
-    indexBox.style.top = `${mouseY - 2 * offset}px`;
+    img.style.position = "fixed";
+    img.style.maxWidth = "50vw";
+    img.style.maxHeight = "90vh";
+    img.style.border = "2px solid gray";
+    img.style.backgroundColor = "#000";
+    img.style.borderRadius = "3px";
+    img.style.zIndex = "1000000";
 
-    if (mouseY - img.height - (2 * offset) <= 0) {
-      img.style.top = `${offset}px`;
+    img.src = postData.url;
+    img.onload = () => this.position(mouseX, mouseY);
+
+    document.body.appendChild(img);
+    this.img = img;
+  }
+
+  hide = () => {
+    document.body.removeChild(this.img);
+  }
+
+  position = (mouseX, mouseY) => {
+    let offset = 20;
+    let img = this.img;
+
+    if (mouseX > window.innerWidth * 0.5) {
+      img.style.left = `${mouseX - img.width - offset}px`;
     } else {
-      img.style.top = `${mouseY - img.height - offset}px`;
+      img.style.left = `${mouseX + offset}px`;
     }
-  } else {
-    indexBox.style.top = `${mouseY + 2 * offset}px`;
 
-    if (mouseY + img.height + (2 * offset) >= window.innerHeight) {
-      img.style.top = `${window.innerHeight - img.height - offset}px`;
+    if (mouseY > window.innerHeight * 0.5) {
+      if (mouseY - img.height - (2 * offset) <= 0) {
+        img.style.top = `${offset}px`;
+      } else {
+        img.style.top = `${mouseY - img.height - offset}px`;
+      }
     } else {
-      img.style.top = `${mouseY + offset}px`;
+      if (mouseY + img.height + (2 * offset) >= window.innerHeight) {
+        img.style.top = `${window.innerHeight - img.height - offset}px`;
+      } else {
+        img.style.top = `${mouseY + offset}px`;
+      }
     }
   }
-};
+}
 
-const supportedSites = ["i.redd.it", "imgur.com"];
+class GalleryHandler {
+  img = null
+  indexBox = null;
 
-const handleMouseEnter = async (e) => {
-  controller = new AbortController();
+  index = 0;
+  images = [];
 
-  const id = e.target.closest("shreddit-post").getAttribute("id");
-  const url = `https://api.reddit.com/by_id/${id}`;
-
-  let res;
-  let jsn;
-  try {
-    res = await fetch(url, { signal: controller.signal });
-    jsn = await res.json();
-  } catch (e) {
-    if (e.name === "AbortError") {
-      return;
+  canHandle = (data) => {
+    if (data.gallery_data && data.media_metadata) {
+      return true;
     }
-    console.error(e);
+
+    return false;
   }
 
-  const data = jsn.data.children[0].data;
-  if (data.gallery_data && data.media_metadata) {
-    // Post is a gallery
-    mediaIndex = 0;
-    mediaImages = [];
-    const metaData = data.media_metadata;
-    const galleryData = data.gallery_data.items;
+  display = (postData, mouseX, mouseY) => {
+    const metaData = postData.media_metadata;
+    const galleryData = postData.gallery_data.items;
+
+    const mediaImages = [];
 
     galleryData.forEach((image) => {
       const meta = metaData[image.media_id];
@@ -90,86 +87,293 @@ const handleMouseEnter = async (e) => {
       const url = `https://i.redd.it/${meta.id}.${type}`;
       mediaImages.push(url);
     });
+    this.mediaImages = mediaImages;
 
-    img.src = mediaImages[mediaIndex];
-    img.hidden = false;
-    if (mediaImages.length > 1) {
-      indexBox.innerText = `${mediaIndex + 1}/${mediaImages.length}`;
-      indexBox.hidden = false;
+    const img = document.createElement("img");
+
+    img.style.position = "fixed";
+    img.style.maxWidth = "50vw";
+    img.style.maxHeight = "90vh";
+    img.style.border = "2px solid gray";
+    img.style.backgroundColor = "#000";
+    img.style.borderRadius = "3px";
+    img.style.zIndex = "1000000";
+
+    img.src = postData.url;
+    img.onload = () => this.position(mouseX, mouseY);
+    document.body.appendChild(img);
+    this.img = img;
+
+    const indexBox = document.createElement("div");
+    indexBox.style.position = "fixed";
+    indexBox.style.backgroundColor = "gray";
+    indexBox.style.padding = "2px 4px";
+    indexBox.style.color = "white";
+    indexBox.style.fontSize = "12px";
+    indexBox.style.textAlign = "center";
+    indexBox.style.zIndex = "1000000";
+    indexBox.innerText = `1/${mediaImages.length}`;
+    document.body.appendChild(indexBox);
+    this.indexBox = indexBox;
+
+    img.src = this.mediaImages[this.index];
+  }
+
+  hide = () => {
+    document.body.removeChild(this.img)
+    document.body.removeChild(this.indexBox)
+
+    this.mediaImages = [];
+    this.index = 0;
+  }
+
+  leftClick = (mouseX, mouseY) => {
+    if (this.index < this.mediaImages.length - 1) {
+      this.index += 1;
+      this.img.src = this.mediaImages[this.index];
+      this.indexBox.innerText = `${this.index + 1}/${this.mediaImages.length}`;
+
+      this.img.onload = () => this.position(mouseX, mouseY)
+
     }
-  } else {
-    // Post is a single image
-    const imgUrl = data.url;
-    let found = false;
-    supportedSites.forEach((site) => {
-      if (imgUrl.includes(site)) {
-        found = true;
-        img.src = imgUrl;
-        img.hidden = false;
+  }
+
+  rightClick = (mouseX, mouseY) => {
+    if (this.index > 0) {
+      this.index -= 1;
+      this.img.src = this.mediaImages[this.index];
+      this.indexBox.innerText = `${this.index + 1}/${this.mediaImages.length}`;
+
+      this.img.onload = () => this.position(mouseX, mouseY)
+    }
+  }
+
+  position = (mouseX, mouseY) => {
+    let offset = 20;
+    let img = this.img;
+    let indexBox = this.indexBox;
+
+    if (mouseX > window.innerWidth * 0.5) {
+      img.style.left = `${mouseX - img.width - offset}px`;
+    } else {
+      img.style.left = `${mouseX + offset}px`;
+    }
+
+    indexBox.style.left = `${mouseX - indexBox.clientWidth / 2}px`;
+
+    if (mouseY > window.innerHeight * 0.5) {
+      indexBox.style.top = `${mouseY - 2 * offset}px`;
+
+      if (mouseY - img.height - (2 * offset) <= 0) {
+        img.style.top = `${offset}px`;
+      } else {
+        img.style.top = `${mouseY - img.height - offset}px`;
       }
-    });
-    if (!found) {
-      console.log("unknown image url:", imgUrl, "id:", id);
+    } else {
+      indexBox.style.top = `${mouseY + 2 * offset}px`;
+
+      if (mouseY + img.height + (2 * offset) >= window.innerHeight) {
+        img.style.top = `${window.innerHeight - img.height - offset}px`;
+      } else {
+        img.style.top = `${mouseY + offset}px`;
+      }
     }
   }
-};
+}
 
-const handleMouseLeave = () => {
-  controller.abort();
-  img.src = "";
-  img.hidden = true;
-  indexBox.hidden = true;
-  mediaIndex = 0;
-  mediaImages = [];
-};
+class RedHandler {
+  frozen = false;
+  width = 0;
+  height = 0;
+  frame = null;
 
-const handleLeftClick = (e) => {
-  e.stopPropagation();
-  e.preventDefault();
+  canHandle = (data) => {
+    if (data.url.includes('www.redgifs.com')) {
+      return true;
+    }
 
-  if (mediaIndex < mediaImages.length - 1) {
-    mediaIndex += 1;
-    img.src = mediaImages[mediaIndex];
-    indexBox.innerText = `${mediaIndex + 1}/${mediaImages.length}`;
+    return false;
   }
-};
 
-const handleRightClick = (e) => {
-  e.preventDefault();
-  e.stopPropagation();
+  display = (postData, mouseX, mouseY) => {
+    if (this.frozen) {
+      return;
+    }
 
-  if (mediaIndex > 0) {
-    mediaIndex -= 1;
-    img.src = mediaImages[mediaIndex];
-    indexBox.innerText = `${mediaIndex + 1}/${mediaImages.length}`;
+    this.width = window.innerWidth / 2;
+    this.height = this.width / (16 / 9);
+
+    const frame = document.createElement("iframe");
+
+    frame.style.position = "fixed";
+    frame.style.width = `${this.width}px`;
+    frame.style.height = `${this.height}px`
+    frame.style.border = "2px solid gray";
+    frame.style.backgroundColor = "#000";
+    frame.style.borderRadius = "3px";
+    frame.scrolling = "no";
+    frame.style.zIndex = "1000000";
+    frame.src = postData.url.replace('watch', 'ifr');
+    frame.onload = () => this.position(mouseX, mouseY);
+    document.body.appendChild(frame);
+    this.frame = frame;
   }
-};
 
-const setupEvents = () => {
-  document.querySelectorAll("shreddit-post").forEach((p) => {
-    const img = p.querySelector('img')
+  hide = () => {
+    if (this.frozen) {
+      return;
+    }
 
-    img.removeEventListener("mouseenter", handleMouseEnter);
-    img.addEventListener("mouseenter", handleMouseEnter);
+    document.body.removeChild(this.frame)
+  }
 
-    img.removeEventListener("mouseleave", handleMouseLeave);
-    img.addEventListener("mouseleave", handleMouseLeave);
+  leftClick = () => {
+    if (this.frozen) {
+      this.frozen = false;
+      this.frame.style.border = "2px solid gray";
+    } else {
+      this.frozen = true;
+      this.frame.style.border = "2px solid red";
+    }
+  }
 
-    img.removeEventListener("click", handleLeftClick);
-    img.addEventListener("click", handleLeftClick);
+  position = (mouseX, mouseY) => {
+    if (this.frozen) {
+      return;
+    }
 
-    img.removeEventListener("contextmenu", handleRightClick);
-    img.addEventListener("contextmenu", handleRightClick);
-  });
-};
+    let offset = 20;
+    let frame = this.frame;
+    let width = this.width;
+    let height = this.height;
 
-img.addEventListener("load", positionImage);
+    if (mouseX > window.innerWidth * 0.5) {
+      frame.style.left = `${mouseX - width - offset}px`;
+    } else {
+      frame.style.left = `${mouseX + offset}px`;
+    }
 
-document.addEventListener("mousemove", (e) => {
-  mouseX = e.clientX;
-  mouseY = e.clientY;
-  positionImage();
-});
+    if (mouseY > window.innerHeight * 0.5) {
+      if (mouseY - height - (2 * offset) <= 0) {
+        frame.style.top = `${offset}px`;
+      } else {
+        frame.style.top = `${mouseY - height - offset}px`;
+      }
+    } else {
+      if (mouseY + height + (2 * offset) >= window.innerHeight) {
+        frame.style.top = `${window.innerHeight - height - offset}px`;
+      } else {
+        frame.style.top = `${mouseY + offset}px`;
+      }
+    }
+  }
+}
 
-setupEvents();
-setInterval(setupEvents, 5000);
+class PopupManager {
+  handlers = [new ImageHandler, new GalleryHandler, new RedHandler]
+  handler = null;
+
+  mouseX = 0;
+  mouseY = 0;
+
+  abortController = null;
+
+  start = () => {
+    this.setupMouseListeners();
+    this.setupImageListeners();
+    setInterval(this.setupImageListeners, 5000);
+  }
+
+  setupMouseListeners = () => {
+    document.addEventListener('mousemove', e => {
+      this.mouseX = e.clientX;
+      this.mouseY = e.clientY;
+
+      if (this.handler) {
+        this.handler.position(this.mouseX, this.mouseY);
+      }
+    })
+  }
+
+  setupImageListeners = () => {
+    document.querySelectorAll("shreddit-post").forEach((p) => {
+      const img = p.querySelector('img')
+
+      try {
+        img.removeEventListener("mouseenter", this.handleMouseEnter);
+        img.removeEventListener("mouseleave", this.handleMouseLeave);
+        img.removeEventListener("click", this.handleLeftClick);
+        img.removeEventListener("contextmenu", this.handleRightClick);
+      } catch (e) { }
+
+      try {
+        img.addEventListener("mouseenter", this.handleMouseEnter);
+        img.addEventListener("mouseleave", this.handleMouseLeave);
+        img.addEventListener("click", this.handleLeftClick);
+        img.addEventListener("contextmenu", this.handleRightClick);
+      } catch (e) { }
+    });
+  }
+
+  handleMouseEnter = async (e) => {
+    this.abortController = new AbortController();
+
+    const id = e.target.closest("shreddit-post").getAttribute("id");
+    const url = `https://api.reddit.com/by_id/${id}`;
+
+    let postData = null;
+    try {
+      const res = await fetch(url, { signal: this.abortController.signal });
+      const jsn = await res.json();
+      postData = jsn.data.children[0].data
+    } catch (e) {
+      if (e.name === "AbortError") {
+        return;
+      }
+      console.error(e);
+      return;
+    }
+
+    for (let handler of this.handlers) {
+      if (handler.canHandle(postData)) {
+        this.handler = handler;
+        this.handler.display(postData, this.mouseX, this.mouseY);
+        return
+      }
+    }
+
+    console.error(`no handlers support ${postData.url}`)
+  }
+
+  handleMouseLeave = () => {
+    if (this.abortController) {
+      this.abortController.abort();
+    }
+
+    if (!this.handler) {
+      return;
+    }
+
+    this.handler.hide();
+    this.handler = null;
+  }
+
+  handleLeftClick = (e) => {
+    if (this.handler && this.handler.leftClick) {
+      e.stopPropagation();
+      e.preventDefault();
+      this.handler.leftClick(this.mouseX, this.mouseY);
+    }
+  }
+
+  handleRightClick = (e) => {
+    if (this.handler && this.handler.rightClick) {
+      e.stopPropagation();
+      e.preventDefault();
+      this.handler.rightClick(this.mouseX, this.mouseY);
+    }
+  }
+}
+
+const manager = new PopupManager();
+manager.start();
