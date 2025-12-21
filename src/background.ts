2316fbd5974527377cb532f6d93b81aea05abe8b
Trevor Hodsdon <mrtj458@gmail.com>
2025-12-21 12:23:43 -0700

    Added image search to context menu

---
+++ src/background.ts
@@ -0,0 +1,17 @@
+/// <reference types="chrome"/>
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.contextMenus.create({
+    title: "Search on SauceNAO",
+    //@ts-ignore
+    contexts: ["image"],
+    id: "searchOnSauceNAO",
+  });
+});
+
+const handleSearch = (info: any) => {
+  const url = new URL("https://saucenao.com/search.php");
+  url.searchParams.set("url", info.srcUrl);
+  chrome.tabs.create({ url: url.toString() });
+};
+
+chrome.contextMenus.onClicked.addListener(handleSearch);
